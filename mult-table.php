eefefd3f4809af49f87fc40253fe4efb933d5973
4rontender <4rontender@gmail.com>
2018-12-06 20:15:37 +0500

    Update mult-table.php

--- mult-table.php
+++ mult-table.php
@@ -1,4 +1,32 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Multiplication Table</title>
+</head>
+<body>
+
+    <style>
+        table {
+            border-collapse: collapse;
+        }
+        td {
+            border: 1px solid #8cd;
+            cursor: pointer;
+            padding: 10px;
+            text-align: center;
+            transition: background-color 0.8s;
+        }
+        td:hover {
+            background-color: #ccc;
+        }
+    </style>
+
+    <h1>Multiplication Table</h1>
+
+    <?php
 
     $rows = 10;
     $cols = 10;
@@ -16,4 +44,7 @@
         $num_r++;
     }
     echo "</table>";
-    
+
+
+</body>
+</html>
